ca2fa0bde4af95202ef87a3d5a0e8e3c34ba15da
Ayan Shaikh <shaikhjiii333@gmail.com>
2026-05-16 17:40:32 +0530

    added

--- src/api/ProtectedRoute.jsx
+++ src/api/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = localStorage.getItem("token"); 
+  const isLoggedIn = localStorage.getItem("authToken"); 
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
