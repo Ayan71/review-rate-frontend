9c90bda7734aa388115eed64d70b70845336d9be
Ayan Shaikh <shaikhjiii333@gmail.com>
2026-05-16 17:30:30 +0530

    edit protected route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,14 +4,31 @@ import Home from "../pages/Home/Home";
 import CompanyDetails from "../pages/CompanyDetails/CompanyDetails";
 import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/Signup";
+import ProtectedRoute from "../api/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/company/:id" element={<CompanyDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/company/:id"
+        element={
+          <ProtectedRoute>
+            <CompanyDetails />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
